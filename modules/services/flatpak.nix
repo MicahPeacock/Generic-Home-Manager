28f38fc57d7d85d486e833ae5dcf2c8c3347a0d2
MicahPeacock <micah.peacock@proton.me>
2025-12-19 15:22:14 -0700

    Fixed imports compilation error in flatpak.nix

--- modules/services/flatpak.nix
+++ modules/services/flatpak.nix
@@ -3,15 +3,15 @@ let
   cfg = config.local.services.flatpak;
 in
 {
+  imports = [
+    nix-flatpak.homeManagerModules.nix-flatpak
+  ];
+
   options.local.services.flatpak = {
     enable = lib.mkEnableOption "Enable usage of Flatpaks";
   };
 
   config = lib.mkIf cfg.enable {
-    imports = [
-      nix-flatpak.homeManagerModules.nix-flatpak
-    ];
-
     services.flatpak = {
       enable = true;
       update.onActivation = true; # Update flatpaks on home-manager switch
