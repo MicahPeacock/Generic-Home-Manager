acd5c7b297ec02cbeb5bd192c5b3c8111bbf1bc4
MicahPeacock <micah.peacock@proton.me>
2026-01-15 17:03:06 -0700

    Fix formatting error.

--- modules/terminal/alacritty.nix
+++ modules/terminal/alacritty.nix
@@ -16,17 +16,15 @@ in
       default = shared.opacity;
       description = "Window opacity to use in Alacritty";
     };
-    font = {
-      name = lib.mkOption {
-        type = lib.types.str;
-        default = shared.font.name;
-        description = "Font family to use in Alacritty";
-      };
-      size = lib.mkOption {
-        type = lib.types.int;
-        default = shared.font.size;
-        description = "Font size (in px) to use in Alacritty";
-      };
+    font.name = lib.mkOption {
+      type = lib.types.str;
+      default = shared.font.name;
+      description = "Font family to use in Alacritty";
+    };
+    font.size = lib.mkOption {
+      type = lib.types.int;
+      default = shared.font.size;
+      description = "Font size (in px) to use in Alacritty";
     };
   };
 
@@ -37,45 +35,43 @@ in
 
       theme = cfg.theme;
       settings = {
-        settings = {
-          general.import = [
-            "general.toml"
-            "keybindings.toml"
-            "options.toml"
-          ];
-        };
+        general.import = [
+          "general.toml"
+          "keybindings.toml"
+          "options.toml"
+        ];
       };
+    };
 
-      home.file = {
-        ".config/alacritty/general.toml".source     = ../../files/.config/alacritty/general.toml;
-        ".config/alacritty/keybindings.toml".source = ../../files/.config/alacritty/keybindings.toml;
-        ".config/alacritty/options.toml".text = ''
-          [terminal]
-          shell = "${config.home.homeDirectory}/.nix-profile/bin/${config.local.programs.shell.default}"
+    home.file = {
+      ".config/alacritty/general.toml".source     = ../../files/.config/alacritty/general.toml;
+      ".config/alacritty/keybindings.toml".source = ../../files/.config/alacritty/keybindings.toml;
+      ".config/alacritty/options.toml".text = ''
+        [terminal]
+        shell = "${config.home.homeDirectory}/.nix-profile/bin/${config.local.programs.shell.default}"
 
-          [window]
-          opacity = ${builtins.toString cfg.opacity}
+        [window]
+        opacity = ${builtins.toString cfg.opacity}
 
-          [font]
-          size = ${builtins.toString cfg.font.size}
+        [font]
+        size = ${builtins.toString cfg.font.size}
 
-          [font.normal]
-          family = "${cfg.font.name}"
-          style = "Normal"
+        [font.normal]
+        family = "${cfg.font.name}"
+        style = "Normal"
 
-          [font.bold]
-          family = "${cfg.font.name}"
-          style = "Bold"
+        [font.bold]
+        family = "${cfg.font.name}"
+        style = "Bold"
 
-          [font.italic]
-          family = "${cfg.font.name}"
-          style = "Italic"
+        [font.italic]
+        family = "${cfg.font.name}"
+        style = "Italic"
 
-          [font.bold_italic]
-          family = "${cfg.font.name}"
-          style = "Bold Italic"
-        '';
-      };
+        [font.bold_italic]
+        family = "${cfg.font.name}"
+        style = "Bold Italic"
+      '';
     };
   };
 }
